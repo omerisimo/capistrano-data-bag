ec5ae752e1005d8924f69c78d1ef9110c8d541fd
Omer Gazit <Omer.Gazit@Klarna.com>
2013-11-17 21:40:49 +0200

    Change the order of files loaded in capistrano-data-bag.rb

--- lib/capistrano-data-bag.rb
+++ lib/capistrano-data-bag.rb
@@ -1,4 +1,4 @@
 require 'capistrano/data_bag/version'
 require 'capistrano/data_bag/support'
-require 'capistrano/data_bag/tasks'
 require 'capistrano/data_bag/dsl'
+require 'capistrano/data_bag/tasks'
